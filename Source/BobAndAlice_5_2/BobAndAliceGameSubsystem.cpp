606d14de99da131d424e203e931177992330ee12
Felixs123 <101900450+Felixs123@users.noreply.github.com>
2023-12-14 14:53:10 -0500

    all done

---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -0,0 +1,38 @@
+﻿#include "BobAndAliceGameSubsystem.h"
+
+void UBobAndAliceGameSubsystem::Initialize(
+    FSubsystemCollectionBase &Collection) {
+  Super::Initialize(Collection);
+}
+
+void UBobAndAliceGameSubsystem::Deinitialize() { Super::Deinitialize(); }
+
+void UBobAndAliceGameSubsystem::SaveIsPickedToSubsystem(bool PickedUp)
+{
+	IsPickedup = PickedUp;
+}
+
+bool UBobAndAliceGameSubsystem::GetLocalSavedPickedUp() const
+{
+	return IsPickedup;
+}
+
+void UBobAndAliceGameSubsystem::SaveTimeToSubsystem(float Time)
+{
+	rTime = Time;
+}
+
+float UBobAndAliceGameSubsystem::GetLocalSavedTime() const
+{
+	return rTime;
+}
+
+void UBobAndAliceGameSubsystem::SaveDeliveredToSubsystem(bool Delivered)
+{
+	IsDelivered = Delivered;
+}
+
+bool UBobAndAliceGameSubsystem::GetLocalSavedDelivered() const
+{
+	return IsDelivered;
+}
---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "BobAndAliceGameSubsystem.generated.h"
+
+UCLASS()
+class BOBANDALICE_5_2_API UBobAndAliceGameSubsystem : public UGameInstanceSubsystem
+{
+  GENERATED_BODY()
+
+public:
+  virtual void Initialize(FSubsystemCollectionBase &Collection) override;
+
+  virtual void Deinitialize() override;
+
+  UFUNCTION(BlueprintCallable)
+  void SaveIsPickedToSubsystem(bool PickedUp);
+  
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  bool GetLocalSavedPickedUp() const;
+
+  UFUNCTION(BlueprintCallable)
+  void SaveTimeToSubsystem(float Time);
+  
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  float GetLocalSavedTime() const;
+
+  UFUNCTION(BlueprintCallable)
+  void SaveDeliveredToSubsystem(bool Delivered);
+  
+  UFUNCTION(BlueprintCallable,BlueprintPure)
+  bool GetLocalSavedDelivered() const;
+
+private:
+  bool IsPickedup;
+  float rTime = 45;
+  bool IsDelivered;
+};
